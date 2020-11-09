ac61ab15fa8bd9671385c0faba85e47db04513f3
dd86185 <DietrichDarciJ@JohnDeere.com>
2020-11-08 21:35:41 -0600

    raod slice

---
+++ settlers_of_deere/src/features/roadNodes.js
@@ -0,0 +1,304 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const roadNodeSlice = createSlice({
+    name: "roadNode",
+    initialState: {
+        roads: [
+            {
+                id: 1,
+                owner: null
+            },
+            {
+                id: 2,
+                owner: null
+            },
+            {
+                id: 3,
+                owner: null
+            },
+            {
+                id: 4,
+                owner: null
+            },
+            {
+                id: 5,
+                owner: null
+            },
+            {
+                id: 6,
+                owner: null
+            },
+            {
+                id: 7,
+                owner: null
+            },
+            {
+                id: 8,
+                owner: null
+            },
+            {
+                id: 9,
+                owner: null
+            },
+            {
+                id: 10,
+                owner: null
+            },
+            {
+                id: 11,
+                owner: null
+            },
+            {
+                id: 12,
+                owner: null
+            },
+            {
+                id: 13,
+                owner: null
+            },
+            {
+                id: 14,
+                owner: null
+            },
+            {
+                id: 15,
+                owner: null
+            },
+            {
+                id: 16,
+                owner: null
+            },
+            {
+                id: 17,
+                owner: null
+            },
+            {
+                id: 18,
+                owner: null
+            },
+            {
+                id: 19,
+                owner: null
+            },
+            {
+                id: 20,
+                owner: null
+            },
+            {
+                id: 21,
+                owner: null
+            },
+            {
+                id: 22,
+                owner: null
+            },
+            {
+                id: 23,
+                owner: null
+            },
+            {
+                id: 24,
+                owner: null
+            },
+            {
+                id: 25,
+                owner: null
+            },
+            {
+                id: 26,
+                owner: null
+            },
+            {
+                id: 27,
+                owner: null
+            },
+            {
+                id: 28,
+                owner: null
+            },
+            {
+                id: 29,
+                owner: null
+            },
+            {
+                id: 30,
+                owner: null
+            },
+            {
+                id: 31,
+                owner: null
+            },
+            {
+                id: 32,
+                owner: null
+            },
+            {
+                id: 33,
+                owner: null
+            },
+            {
+                id: 34,
+                owner: null
+            },
+            {
+                id: 35,
+                owner: null
+            },
+            {
+                id: 36,
+                owner: null
+            },
+            {
+                id: 37,
+                owner: null
+            },
+            {
+                id: 38,
+                owner: null
+            },
+            {
+                id: 39,
+                owner: null
+            },
+            {
+                id: 40,
+                owner: null
+            },
+            {
+                id: 41,
+                owner: null
+            },
+            {
+                id: 42,
+                owner: null
+            },
+            {
+                id: 43,
+                owner: null
+            },
+            {
+                id: 44,
+                owner: null
+            },
+            {
+                id: 45,
+                owner: null
+            },
+            {
+                id: 46,
+                owner: null
+            },
+            {
+                id: 47,
+                owner: null
+            },
+            {
+                id: 48,
+                owner: null
+            },
+            {
+                id: 49,
+                owner: null
+            },
+            {
+                id: 50,
+                owner: null
+            },
+            {
+                id: 51,
+                owner: null
+            },
+            {
+                id: 52,
+                owner: null
+            },
+            {
+                id: 53,
+                owner: null
+            },
+            {
+                id: 54,
+                owner: null
+            },
+            {
+                id: 55,
+                owner: null
+            },
+            {
+                id: 56,
+                owner: null
+            },
+            {
+                id: 57,
+                owner: null
+            },
+            {
+                id: 58,
+                owner: null
+            },
+            {
+                id: 59,
+                owner: null
+            },
+            {
+                id: 60,
+                owner: null
+            },
+            {
+                id: 61,
+                owner: null
+            },
+            {
+                id: 62,
+                owner: null
+            },
+            {
+                id: 63,
+                owner: null
+            },
+            {
+                id: 64,
+                owner: null
+            },
+            {
+                id: 65,
+                owner: null
+            },
+            {
+                id: 66,
+                owner: null
+            },
+            {
+                id: 67,
+                owner: null
+            },
+            {
+                id: 68,
+                owner: null
+            },
+            {
+                id: 69,
+                owner: null
+            },
+            {
+                id: 70,
+                owner: null
+            },
+            {
+                id: 71,
+                owner: null
+            },
+            {
+                id: 72,
+                owner: null
+            }
+        ]
+    },
+    reducers: {
+
+    }
+})
+
+export const roads = state => state.roadNodeSlice.roads;
+
+export default roadNodeSlice.reducer;
