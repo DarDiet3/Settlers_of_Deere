441f701b018451c2a71a614e63cd4a3e21a0257f
dd86185 <DietrichDarciJ@JohnDeere.com>
2020-11-11 09:02:48 -0600

    added use reducer. take care of state change in hook

--- settlers_of_deere/src/App.js
+++ settlers_of_deere/src/App.js
@@ -1,10 +1,18 @@
 import { Redirect, Route, Switch } from 'react-router';
+import { useSelector, useDispatch} from 'react-redux';
 
+import {currentUser, setCurrentUser} from "./features/gameMetaDataSlice";
 import Gameboard from "./components/GameBoard";
+import LoginForm from "./components/LoginForm"; 
+import { signupUser, loginUser, VerifyUser} from "./services/api_helper";
+
 
 function App() {
   return (
     <div className="App">
+      <Route path="/signup" render={() => (
+        <LoginForm/>
+      )} />
       <Gameboard/>
     </div>
   );
--- settlers_of_deere/src/features/gameMetaDataSlice.js
+++ settlers_of_deere/src/features/gameMetaDataSlice.js
@@ -6,7 +6,8 @@ export const gameMetaDataSlice = createSlice({
         dice: [1, 1],
         dice_total: 2,
         number_tokens: [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10 ,10, 11, 11, 12],
-        resource_hexes: ["brick", "brick", "brick", "wood", "wood", "wood", "wood", "rock", "rock", "rock", "wheat", "wheat", "wheat", "wheat", "sheep", "sheep", "sheep", "sheep"]
+        resource_hexes: ["brick", "brick", "brick", "wood", "wood", "wood", "wood", "rock", "rock", "rock", "wheat", "wheat", "wheat", "wheat", "sheep", "sheep", "sheep", "sheep"],
+        currentUser: ""
     },
     reducers: {
         setDice: (state, action) => {
@@ -19,13 +20,18 @@ export const gameMetaDataSlice = createSlice({
         }, 
         setResourceHexes: (state, action) => {
             state.resource_hexes = action.payload;
-        }
+        }, 
+        setCurrentUser: (state, action) => {
+            state.currentUser = action.payload;
+        },
+        
     }
 })
 
-export const { setDice, setTokens, setResourceHexes, rollDice } = gameMetaDataSlice.actions;
+export const { setDice, setTokens, setResourceHexes, rollDice, setCurrentUser } = gameMetaDataSlice.actions;
 
 export const dice = state => state.gameData.dice;
 export const tokens = state => state.gameData.number_tokens;
 export const resource_hexes = state => state.gameData.resource_hexes;
+export const currentUser = state => state.gameData.currentUser;
 export default gameMetaDataSlice.reducer;
