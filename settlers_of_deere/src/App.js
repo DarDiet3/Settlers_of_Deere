605476f63b2edc79fff52a3ce82a14d22d9c547a
dd86185 <DietrichDarciJ@JohnDeere.com>
2020-11-12 00:18:29 -0600

    got logina dn redirect to work

--- settlers_of_deere/src/App.js
+++ settlers_of_deere/src/App.js
@@ -3,17 +3,24 @@ import { useSelector, useDispatch} from 'react-redux';
 
 import {currentUser, setCurrentUser} from "./features/gameMetaDataSlice";
 import Gameboard from "./components/GameBoard";
-import LoginForm from "./components/LoginForm"; 
+import LoginForm from "./components/SignUpForm"; 
 import { signupUser, loginUser, VerifyUser} from "./services/api_helper";
+import LandingPage from './components/LandingPage';
 
 
 function App() {
+  console.log(currentUser ? "yes" : "no")
   return (
     <div className="App">
-      <Route path="/signup" render={() => (
-        <LoginForm/>
-      )} />
-      <Gameboard/>
+      {currentUser ? 
+
+        <Gameboard/>
+        :
+        <div>
+          <Route exact path="/" component={LandingPage}/>
+          <Route path="/signup" component={LoginForm}/>
+        </div>
+    }
     </div>
   );
 }
